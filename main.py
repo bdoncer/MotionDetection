2d53c34f82043f7875f4e01f92679b6df2a4aece
Dorota <dorota.meszka@gmail.com>
2022-06-01 17:48:51 +0200

    to chyba jest zle ale wole miec kopie

--- main.py
+++ main.py
@@ -29,12 +29,14 @@ if __name__ == '__main__':
         start_y = 0
         pref_width = -1
         pref_height = -1
+    print(start_x, start_x + pref_width, start_y, start_y + pref_height)
 
     prev_frame = None
     cnt = 0
     while 1:
         frame = video.read()[1]
-
+        full_width = len(frame[0])
+        full_height = len(frame)
         if pref_height == -1:
             pref_width = len(frame[0])
             pref_height = len(frame)
@@ -49,19 +51,20 @@ if __name__ == '__main__':
 
         if prev_frame is not None:
             cv2.accumulateWeighted(gray_frame, prev_frame, 0.05)
-
             diff_frame = cv2.absdiff(cv2.convertScaleAbs(prev_frame), gray_frame)
             thresh_frame = cv2.threshold(diff_frame, sens, 255, cv2.THRESH_BINARY)[1]
             thresh_frame = cv2.dilate(thresh_frame, None, iterations=1)
             contour_frame = thresh_frame.copy()
-            contour_frame = cv2.resize(contour_frame, (pref_width, pref_height))
             moves = cv2.findContours(contour_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
             for move in moves:
                 (x, y, w, h) = cv2.boundingRect(move)
-                if cv2.contourArea(move) < 1000:
-                    continue
-                else:
-                    cv2.rectangle(frame, (int(contour_width[0] + x), int(contour_height[0] + y)), (int(contour_width[0] + x + w), int(contour_height[0] + y + h)), (128, 0, 200), 2)
+                if (cv2.contourArea(move) > 1000 and upgrade != 'y') or (cv2.contourArea(move) > 1000
+                       and upgrade == 'y' and x >= start_x and x <= start_x + pref_width and y >= start_y and y <= start_y + pref_height
+                       and x + w >= start_x and x + w <= start_x + pref_width and y + h >= start_y and y + h < start_y + pref_height):
+                    print(x, y, x+w, y-h)
+                    print(start_x, full_height - start_y, start_x + pref_width, full_height - start_y + pref_height)
+                    cv2.rectangle(frame, (int(contour_width[0] + x), int(contour_height[0] + y)),
+                                  (int(contour_width[0] + x + w), int(contour_height[0] + y + h)), (128, 0, 200), 2)
             if is_debug == "y":
                 display_frames(gray_frame, diff_frame, thresh_frame)
             cv2.imshow("Color Frame", frame)
